56adcbeada00cdf7b8e4fa9294e0600026b3e48a
randalstoddard <randalstoddard@mail.weber.edu>
2018-08-04 15:47:52 -0600

    intital commit for class GuessesByPlayer

---
+++ src/GuessesByPlayer.java
@@ -0,0 +1,19 @@
+public class GuessesByPlayer
+{
+    private int [][] guessesByPlayer;
+
+    public GuessesByPlayer(int[][] guessesByPlayer)
+    {
+        this.guessesByPlayer = guessesByPlayer;
+    }
+
+    public int[][] getGuessesByPlayer()
+    {
+        return guessesByPlayer;
+    }
+
+    public void setGuessesByPlayer(int[][] guessesByPlayer)
+    {
+        this.guessesByPlayer = guessesByPlayer;
+    }
+}
