14b8a679984719d98b7dbafce774b7f468919d11
original commit <>
2018-08-05 02:04:26 -0600

    create GameHandler with objects

---
+++ src/GameHandler.java
@@ -0,0 +1,17 @@
+public class GameHandler {
+    public Game game;
+
+    public Player player1;
+    public Player player2;
+
+    public Gameboard player1Gameboard;
+    public Gameboard player2Gameboard;
+
+    public Fleet player1Fleet;
+    public Fleet player2Fleet;
+
+    public GuessesByPlayer player1Guesses;
+    public GuessesByPlayer player2Guesses;
+
+    
+}
